672b7f897a3394a5e5740a80a022e90ae344bc12
Akshit Gupta <62721801+akshit-mee@users.noreply.github.com>
2020-04-13 10:53:46 +0530

    Add files via upload

---
+++ Tweeter-Scrape-Slack-Serve/Bot_v3.py
@@ -0,0 +1,77 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+import json
+from slacker import Slacker
+slack = Slacker('xoxb--1056560388466-euaQDZpyaHrUAjZRpQnpMKyE')
+
+def get_tweet_text(tweet):
+    tweet_text_box = tweet.find("p", {"class": "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"})
+    images_in_tweet_tag = tweet_text_box.find_all("a", {"class": "twitter-timeline-link u-hidden"})
+    tweet_text = tweet_text_box.text
+    for image_in_tweet_tag in images_in_tweet_tag:
+        tweet_text = tweet_text.replace(image_in_tweet_tag.text, '')
+
+    return tweet_text
+
+def get_this_page_tweets(soup):
+    tweets_list = list()
+    tweets = soup.find_all("li", {"data-item-type": "tweet"})
+    for tweet in tweets:
+        tweet_data = None
+        tweet_data = get_tweet_text(tweet)
+        if tweet_data:
+            tweets_list.append(tweet_data)
+            slack.chat.post_message('#bot', tweet_data)
+            sys.stdout.flush()
+
+    return tweets_list
+
+
+def get_tweets_data(username, soup):
+    tweets_list = list()
+    tweets_list.extend(get_this_page_tweets(soup))
+
+    next_pointer = soup.find("div", {"class": "stream-container"})["data-min-position"]
+    #for loading more tweets/ scroll down
+##    while True:
+##        next_url = "https://twitter.com/i/profiles/show/" + username + \
+##                   "/timeline/tweets?include_available_features=1&" \
+##                   "include_entities=1&max_position=" + next_pointer + "&reset_error_state=false"
+##
+##        next_response = None
+##        try:
+##            next_response = requests.get(next_url)
+##        except Exception as e:
+##            # in case there is some issue with request. None encountered so far.
+##            print(e)
+##            return tweets_list
+##
+##        tweets_data = next_response.text
+##        tweets_obj = json.loads(tweets_data)
+##        if not tweets_obj["has_more_items"] and not tweets_obj["min_position"]:
+##            # using two checks here bcz in one case has_more_items was false but there were more items
+##            print("\nNo more tweets returned")
+##            break
+##        next_pointer = tweets_obj["min_position"]
+##        html = tweets_obj["items_html"]
+##        soup = BeautifulSoup(html, 'lxml')
+##        tweets_list.extend(get_this_page_tweets(soup))
+
+    return tweets_list
+
+
+def function():
+    username = 'Every3Minutes'
+    url = "http://www.twitter.com/every3minutes"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    if soup.find("div", {"class": "errorpage-topbar"}):
+        print("\n\n Error: Invalid username.")
+        sys.exit(1)
+
+    tweets = get_tweets_data(username, soup)
+    print(tweets)
+
+function()
